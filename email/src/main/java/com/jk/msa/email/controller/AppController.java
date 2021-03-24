1c8908dc12158786dc0f4606746bb7949e1cde0a
jinkyuhan <gkswlsrb95@gmail.com>
2021-03-25 00:38:20 +0900

    fix: Mail service setting

--- email/src/main/java/com/jk/msa/email/controller/AppController.java
+++ email/src/main/java/com/jk/msa/email/controller/AppController.java
@@ -1,4 +1,4 @@
-package com.jk.msa.email;
+package com.jk.msa.email.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +8,6 @@ public class AppController {
 
   @GetMapping
   public String checkHealth() {
-    return "I'm alive!";
+    return "I'm alive@@@";
   }
 }
--- email/src/main/java/com/jk/msa/email/entity/Mail.java
+++ email/src/main/java/com/jk/msa/email/entity/Mail.java
@@ -1,17 +1,21 @@
-package com.jk.msa.email;
+package com.jk.msa.email.entity;
 
 import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
+@Table()
 public class Mail {
 
   @Id
